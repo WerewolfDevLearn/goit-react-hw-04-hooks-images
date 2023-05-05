50ee6fa5508e2086130cc3ddfecdacd26880cf6c
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-05-05 10:15:52 +0300

    fix problem with modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,36 +1,40 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useRef, useEffect } from 'react';
 import { ModaLProps } from '../interfaces/interfaces';
 export default function Modal({ onCloseModal, children }: ModaLProps) {
+  const overlayRef = useRef<HTMLHeadingElement>(null);
   const pressEscBtn = useCallback(
-    (event: KeyboardEvent) => {
-      // console.log(event);
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      console.log(event);
       if (event.code === 'Escape') {
         onCloseModal();
       }
     },
     [onCloseModal],
   );
-  useEffect(() => {
-    window.addEventListener('keydown', (e) => pressEscBtn(e));
-    return () => {
-      window.removeEventListener('keydown', pressEscBtn);
-    };
-  }, [pressEscBtn]);
   const onOverlayClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (event.target === event.currentTarget) onCloseModal();
   };
+  useEffect(() => {
+    overlayRef.current?.focus();
+  }, []);
+
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
       className='Overlay'
       onClick={onOverlayClick}
-      // onKeyDown={pressEscBtn}
-      // role='button'
-      // tabIndex={0}
-      // ref={overlayRef}
+      onKeyDown={pressEscBtn}
+      role='button'
+      tabIndex={0}
+      ref={overlayRef}
     >
       <div className='Modal'>{children}</div>
     </div>
   );
 }
 // onKeyDown={pressEscBtn} tabIndex={0} role='button'
+// useEffect(() => {
+//   window.addEventListener('keydown', (e) => pressEscBtn(e));
+//   return () => {
+//     window.removeEventListener('keydown', pressEscBtn);
+//   };
+// }, [pressEscBtn]);
